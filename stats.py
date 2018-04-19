800c3d32ef2fc6aba58497234b0fe7a6b70b06ff
Rachad <rachadnouiga@pal-nat186-105-157.itap.purdue.edu>
2018-04-19 19:48:55 -0400

    test for branches

--- stats.py
+++ stats.py
@@ -1,10 +1,23 @@
 import sys
 
+mean = 0.0
+
 for elem in sys.argv[1:]:
 	mean =  mean + float(elem)
 
 mean = mean / float(len(sys.argv)-1)
-print(mean)
+
+var = 0.0
+for elem in sys.argv[1:]:
+	var = var + (float(elem) - mean)**2
+
+var = var / (len(sys.argv) - 2)
+
+print("Mean: ",  mean)
+print("Variance: ", var)
+
+
+print("Hello World")
 
 
 
